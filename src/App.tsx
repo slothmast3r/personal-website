e1fa4b823aba1876779cad4c241bdfe487ac21ce
Oskar Straszynski <oskar.straszynski@gmail.com>
2023-06-26 23:51:22 +0200

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import MainPage from './assets/pages/MainPage'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-
-       </div>
-
+    <MainPage />
     </>
   )
 }
