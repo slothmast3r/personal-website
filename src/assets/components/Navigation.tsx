7cafd410f338e5e749909ed83c571741589737dc
Oskar <oskar.straszynski@gmail.com>
2023-07-04 00:58:55 +0200

    adding slider

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import './Navigation.scss'
-
-
+import { useEffect, useState } from "react";
 
 const locations = [
   {
@@ -23,18 +21,45 @@ const locations = [
 ]
 
 function Navigation(){
+  const [activePath, setActivePath] = useState(window.location.hash);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setActivePath(window.location.hash);
+    };
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    const hash = event.currentTarget.hash;
+    const target = document.querySelector(hash);
+    if (target) {
+      const offset = target.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth"
+      });
+    }
+    window.location.hash = hash;
+
+  };
+
   return (
     <nav className="nav">
       <a href="/" className="site-logo">Site</a>
       <ul>
         {locations.map(location => (
-          <li key={location.path}>
-            <a href={location.path}>{location.name}</a>
+          <li key={location.path} className={location.path === activePath ? "active" : ""}>
+            <a href={location.path} onClick={handleClick}>{location.name}</a>
           </li>
         ))}
       </ul>
     </nav>
   )
-};
+}
 
 export default Navigation;
--- src/assets/pages/MainPage.tsx
+++ src/assets/pages/MainPage.tsx
@@ -1,13 +1,43 @@
-import Navigaiton from "../components/Navigation";
+import Slider from "react-slick";
+import './MainPage.css'; // import CSS file
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed:1500,
+    arrows: false,
+  };
+function MainPage() {
 
-function MainPage(){
-
-    return(
+    return (
         <>
-            <h1>Main Page</h1>
+        <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
         </>
-    )
+    );
 }
 
 export default MainPage;
