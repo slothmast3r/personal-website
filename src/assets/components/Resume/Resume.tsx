9b056a0517523dbef77dcbbab944fa74dc337da5
Oskar <oskar.straszynski@gmail.com>
2023-07-05 17:39:31 +0200

    resume blocks

--- src/assets/components/Resume/Resume.tsx
+++ src/assets/components/Resume/Resume.tsx
@@ -1,9 +1,14 @@
+import ResumeBlock from "../ResumeBlock/ResumeBlock";
 import "./Resume.scss";
 export default function Resume() {
   return (
-    <div className="text-container">
-      <h3 className="small-text-bg">Resume</h3>
-      <h4 className="big-text-bg">Resume</h4>
-    </div>
+    <>
+      <div className="text-container">
+        <h3 className="small-text-bg">Resume</h3>
+        <h4 className="big-text-bg">Resume</h4>
+      </div>
+      <ResumeBlock></ResumeBlock>
+        
+    </>
   );
 }
