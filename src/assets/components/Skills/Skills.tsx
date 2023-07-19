68a29c3555f8028aee18f3792ca8c0d129aa5094
Oskar <oskar.straszynski@gmail.com>
2023-07-19 11:15:36 +0200

    progres bar

--- src/assets/components/Skills/Skills.tsx
+++ src/assets/components/Skills/Skills.tsx
@@ -1,12 +1,21 @@
-import { ProgressBar } from "../ProgressBar"
-import "./Skills.scss"
+import { ProgressBar } from "../ProgressBar";
+import "./Skills.scss";
 
-
-export default function Skills(){
-    return (<div id="skills"><div className="text-container">
-    <h3 className="small-text-bg">My Skills</h3>
-    <h4 className="big-text-bg">Skills</h4>
-
-    <ProgressBar title="Marketing" percentage={75}></ProgressBar>
-  </div></div>)
+export default function Skills() {
+  return (
+    <div id="skills">
+      <div className="text-container">
+        <h3 className="small-text-bg">My Skills</h3>
+        <h4 className="big-text-bg">Skills</h4>
+      </div>
+      <div>
+      <ProgressBar title="Marketing" percentage={75}></ProgressBar>
+      <ProgressBar title="Marketing" percentage={75}></ProgressBar>
+      <ProgressBar title="Marketing" percentage={75}></ProgressBar>
+      <ProgressBar title="Marketing" percentage={75}></ProgressBar>
+      <ProgressBar title="Marketing" percentage={75}></ProgressBar>
+      <ProgressBar title="Marketing" percentage={75}></ProgressBar>
+      </div>
+    </div>
+  );
 }
