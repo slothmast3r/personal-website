23d4d6c490f5189e15b663190e5553ad0ec4fd6f
Oskar <oskar.straszynski@gmail.com>
2023-08-06 15:19:45 +0200

    tbody

--- src/assets/components/AboutMe/AboutMe.tsx
+++ src/assets/components/AboutMe/AboutMe.tsx
@@ -17,22 +17,24 @@ export default function AboutMe() {
           doloribus quidem voluptate.
         </p>
         <table>
-          <tr>
-            <th>Name</th>
-            <td>Oskar Straszynski</td>
-          </tr>
-          <tr>
-            <th>Email</th>
-            <td>oskar@straszynski.pl</td>
-          </tr>
-          <tr>
-            <th>Phone</th>
-            <td>577966612</td>
-          </tr>
-          <tr>
-            <th>GitHub URL</th>
-            <td>slothmast3r</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <td>Oskar Straszynski</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>oskar@straszynski.pl</td>
+            </tr>
+            <tr>
+              <th>Phone</th>
+              <td>577966612</td>
+            </tr>
+            <tr>
+              <th>GitHub URL</th>
+              <td>slothmast3r</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
